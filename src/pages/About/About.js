8c17f8ac72ca73f94ab21ca139edf48fa9977cec
farid5630 <mdforidulislam5630@gmail.com>
2022-04-18 22:43:29 +0600

    about page complete

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='w-50 mx-auto'>
-            <h4>This is Md Faridul islam</h4>
-            
-
-        </div>
+      <div className="w-50 mx-auto">
+        <h4>This is Md Faridul islam</h4>
+        <h5>Address: Home Town Kurigram District</h5>
+        <h5>Profession: Student</h5>
+        <h5>Interested: Technology</h5>
+        <h6>favorite: মায়ের হাতের রান্না </h6>
+            <br />
+            <p>Dream: </p>
+      </div>
     );
 };
 
